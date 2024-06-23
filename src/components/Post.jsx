3dcfa5ca50b1ac269c05552481684b24e88a444f
Thauler Ferreira Mayrink <thauler@MacBook-Pro-de-Thauler.local>
2024-06-22 21:05:13 -0300

    style(post): add CSS to post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,30 +2,27 @@ import styles from './Post.module.css';
 
 export function Post(props) {
   return (
-  <article className={styles.post}>
-    <header>
-      <div
-        className={styles.author}
-      >
-        <img
-          className={styles.avatar}
-          src="https://github.com/Thauler.png"
-          alt="Author"/>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/Thauler.png"/>
           <div className={styles.authorInfo}>
             <strong>{props.author}</strong>
             <span>{props.authorInfo}</span>
           </div>
-      </div>
+        </div>
 
-      <time title='data exata' dateTime={Date.now()} className={styles.postDate}>Publicado há 1h</time>
-
-      <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉 jane.design/doctorcare</p>
-        <p>#novoprojeto #nlw #rocketseat</p>
-      </div>
-    </header>
-  </article>
+        <time title='data exata' dateTime={Date.now()} className={styles.postDate}>Publicado há 1h</time>
+      </header>
+        <div className={styles.content}>
+          <p>Fala galeraa 👋</p>
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+          <p>👉<a href='#'>jane.design/doctorcare</a></p>
+          <p>
+            <a href='#'>#novoprojeto</a>{" "}
+            <a href='#'>#nlw</a>{" "}
+            <a href='#'>#rocketseat</a></p>
+        </div>
+    </article>
 )
 }
